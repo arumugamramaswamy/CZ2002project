ff8f4524d7a4038f17f74bff94a7b2adb83ccdfc
arumugam666 <44291537+arumugam666@users.noreply.github.com>
2019-11-04 18:26:40 +0800

    test

---
+++ movie.java
@@ -0,0 +1,170 @@
+package assignment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class movie {
+	
+	// name of the movie
+	private String movieName;
+	
+	// create a common class PERSON which contains first name and last name
+	// movie directors name - object of class PERSON
+	private String directorName;
+	
+	// all movie reviews -- Array of class AllReviews which contains all reviews and person details with PERSON class
+	private String[] reviews;
+	
+	// all movie ratings -- Array of class AllRatings and person details with PERSON class
+	private double[] ratings;
+	
+	// status of the movie - preview, now showing, coming soon
+	private String showingStatus;
+	
+	// synopsis of the movie
+	private String Synopsis;
+	
+	// cast of the movie -- Array of PERSON class type (can have 2 - 3 actors)
+	private String[] Cast;
+	
+	// all shows of the movie
+	private show[] Shows;
+	
+	
+	// main constructor
+	public movie(String name,String director,String[] reviews, double[] ratings,
+								String showingStatus, String Synopsis, String[] Cast) {
+		movieName = name;
+		directorName = director;
+		this.Cast = Cast;
+		this.Synopsis = Synopsis;
+		this.showingStatus = showingStatus;
+		this.ratings = ratings;
+		this.reviews = reviews;
+	}
+	
+	// constructor used by show
+	public movie(movie mov) {
+		movieName = mov.movieName;
+		directorName = mov.directorName;
+		reviews = mov.reviews;
+		ratings = mov.ratings;
+		showingStatus = mov.showingStatus; // preview, now showing, coming soon
+		Synopsis = mov.Synopsis;
+		Cast = mov.Cast;
+	}
+	
+	public String getMovieName() {
+		return movieName;
+	}
+	
+	public String getShowingStatus() {
+		return showingStatus;
+	}
+	
+	public String getSynopsis() {
+		return Synopsis;
+	}
+	
+	public String getDirectorName() {
+		return directorName;
+	}
+	
+	public String[] getCast() {
+		return Cast;
+	}
+	
+	public double getTotalRating() {
+		if (ratings.length == 0)
+			return -1;
+		double sum = 0;
+		for (int i = 0;i<ratings.length;i++) {
+			sum+=ratings[i];
+		}
+		sum/=ratings.length;
+		return sum;
+	}
+	
+	public double[] getAllRatings() {
+		return ratings;
+	}
+	
+	public String[] getReviews() {
+		return reviews;
+	}
+	
+	public void writeReview(String rev) {
+		String[] temp;
+		if (reviews == null) {
+			temp = new String[1];
+			temp[0] = rev;
+			}
+		else {
+		temp = new String[reviews.length+1];
+		for (int i=0;i<reviews.length;i++) {
+			temp[i] = reviews[i];
+		}
+		temp[reviews.length] = rev;
+	}
+		reviews = temp;
+	}
+	
+	public void giveRating(double rat) {
+		double[] temp;
+		if(ratings == null) {
+			temp = new double[1];
+			temp[0] = rat;
+		}
+		else{
+			temp = new double[ratings.length+1];
+		for (int i=0;i<ratings.length;i++) {
+			temp[i] = ratings[i];
+		}
+		temp[ratings.length] = rat;
+	}
+		ratings = temp;
+	}
+	
+	//create a show for this movie, currently set to protected to be used by admin module
+	void createShowListing(String dt,int screenNum,int cineplexID, boolean is3D) {
+		show[] temp;
+		show s = new show(this,dt,screenNum,cineplexID,is3D);
+		if(Shows == null) {
+			temp = new show[1];
+			temp[0] = s;
+		}else {
+		temp = new show[Shows.length+1];
+		for (int i=0;i<Shows.length;i++) {
+			temp[i] = Shows[i];
+		}
+		temp[Shows.length] = s;
+		}
+		Shows = temp;
+	}
+	
+	//save all shows to a text file under the Shows folder with file name as the movie name
+	void saveShowDetails() {
+		movieIO m = new movieIO();
+		try {
+		m.saveShows("Shows/"+movieName+".txt", Shows);
+		}catch(IOException e) {
+			System.out.println("IOException > " + e.getMessage());
+		}
+	}
+	
+	//read show details from the shows folder with file name as movie name
+	void readShowDetails() {
+		movieIO m = new movieIO();
+		try {
+		ArrayList arr = m.readShows(this,"Shows/"+movieName+".txt");
+		Shows = new show[arr.size()];
+		for(int i =0;i<arr.size();i++) {
+			Shows[i] = (show)arr.get(i);
+		}
+			
+		}catch(IOException e) {
+			System.out.println("IOException > " + e.getMessage());
+		}
+	}
+	
+}
---
+++ tester.java
@@ -0,0 +1,93 @@
+package assignment;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+public class tester {
+
+	
+	public static void main(String[] args) {
+		movie m = new movie("YOYO","supsup?", null, null, null, null, args);
+//		m.readShowDetails();
+//		m.saveShowDetails();
+		show s = new show(m,"10/10/19 12:00:00", 0, 0,false);
+		s.printSeats();
+////		s.assignSeat(0, 0);
+////		System.out.print("\n");
+////		s.printSeats();
+////		m.writeReview("YOYOYO!!!");
+////		m.writeReview("Hi");
+////		m.giveRating(10);
+////		for (String str:m.getReviews()) {
+////			System.out.println(str);
+////		}
+////		for (double d:m.getAllRatings()) {
+////			System.out.println(d);
+////		}
+//		//read and write read object + extra movie entry
+//		ArrayList arr = new ArrayList();
+//		movieIO w = new movieIO();
+////		
+////		//code to read comments.txt and extract movie data
+////		try {
+////		arr = w.readMovie("comments.txt");
+////		}
+////		catch (IOException e){
+////			System.out.println("IOException > " + e.getMessage());
+////		}
+////		String[] Cast = {"yoo","yo"};
+////		double[] rats = {4};
+////		String[] revs = {"Great"};
+////		movie mov =new movie("asuran","siddy",revs,rats,"now showing","asuran daw",Cast);
+////		arr.add(mov);
+////		try {
+////			w.saveMovie("comments.txt", arr);
+////		}catch(IOException e){
+////			System.out.println("IOException > " + e.getMessage());
+////		}
+//		
+//		//read and write comment to read object
+//		
+////		ArrayList alw = new ArrayList();
+////		for (Object obj:arr) {
+////			movie mov = (movie)obj;
+////			mov.giveRating(0.0);
+////			alw.add(mov);
+////		}
+////		try {
+////			w.saveMovie("comments.txt", arr);
+////		}catch(IOException e){
+////			System.out.println("IOException > " + e.getMessage());
+////		}
+//		
+//		try {
+//		arr = w.readShows(m,"YOYO.txt");
+//		}
+//		catch (IOException e){
+//			System.out.println("IOException > " + e.getMessage());
+//		}
+//		show[] Shows = new show[arr.size()];
+//		for (int i =0; i<arr.size(); i++) {
+//			Shows[i] = (show)arr.get(i);
+//		}
+////		
+////		try {
+////			w.saveShows("YOYO.txt",Shows);
+////		}
+////		catch (IOException e){
+////			System.out.println("IOException > " + e.getMessage());
+////		}
+////		try {
+////		PrintWriter writer = new PrintWriter(new PrintWriter(new FileWriter("movie.txt")));
+////		writer.println("10");
+////		writer.close();
+////		}
+////		catch(IOException e) {
+////			System.out.println("IOException > " + e.getMessage());
+////		}
+
+		
+	}
+}
