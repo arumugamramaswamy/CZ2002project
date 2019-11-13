54bb54282464ef3b7047e54b20db15c5996bb754
Jay Gupta <jaygupta2607@gmail.com>
2019-11-13 19:52:26 +0800

    Auto stash before merge of "master" and "origin/master"

--- assignment/src/model/Cinema.java
+++ assignment/src/model/Cinema.java
@@ -2,16 +2,36 @@ package model;
 
 import java.util.ArrayList;
 
-
+/**
+ * Class to create Cinemas in Cineplexes and initialize shows in it
+ * @version 1.0
+ */
 
 
 
 public class Cinema {
+	
+	/**
+	 * Name of Cinema
+	 */
     private String cinemaName;
+    
+    /**
+     * Class of Cinema
+     */
     private String cinemaClass;
+    
+    /**
+     * ID of Cinema
+     */
     private int cinemaID;
+    
+    /**
+     * Array of all the shows in the Cinema
+     */
     private ArrayList<show> showList;
 
+    // REMOVE??
 
 //    public Cinema() {
 //        cinemaName="";
@@ -21,10 +41,13 @@ public class Cinema {
 //      
 //    }
 //
-
-
-
-  
+    
+    /**
+     * Default Constructor
+     * @param cinemaName 	name of the Cinema
+     * @param cinemaClass	class of the Cinema
+     * @param cinemaID		ID of the Cinema
+     */
     public Cinema(String cinemaName, String cinemaClass, int cinemaID) {
         this.cinemaName = cinemaName;
         this.cinemaClass = cinemaClass;
@@ -33,6 +56,9 @@ public class Cinema {
 
         
     }
+    
+    // REMOVE??
+    
 //  public Cinema(String cinemaName, String cinemaClass, String cinemaCode, ArrayList<MovieInCinema> moviesList) {
 //        this.cinemaName = cinemaName;
 //        this.cinemaClass = cinemaClass;
@@ -41,34 +67,46 @@ public class Cinema {
 //        
 //    }
  
-
+    /**
+     * @return Name of the cinema
+     */
     public String getCinemaName() {
         return cinemaName;
     }
 
-
+    /**
+     * @return Class of the cinema
+     */
     public String getCinemaClass() {
         return cinemaClass;
     }
 
-
-
+    /**
+     * @return ID of the Cinema
+     */
     public int getCinemaID() {
         return cinemaID;
     }
 
+    /**
+     * Add a show to the cinema
+     * @param s add a particular show to the cinema
+     */
     public void addShow(show s) {
     	showList.add(s);
     }
  
+    /**
+     * @return array of all shows in the Cinema
+     */
     public ArrayList<show> getCinemaShows(){
     	return showList;
     }
     
+    // REMOEV ??
 //    public ArrayList<MovieInCinema> getMoviesList() {
 //        return moviesList;
 //    }
-
-   
+ 
 }
 
